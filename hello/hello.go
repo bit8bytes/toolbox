6d9d88545e6b3a4f6ad73cf099bf2eb5863cdeb2
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-11-30 19:36:07 +0100

    hello: changes for v0.1.0

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,5 @@ package hello
 var Purpose = "We exist to build great things"
 
 func Greet(name string) string {
-	return "Hello, " + name + "!"
+	return "Hello, " + name + "! Happy to have you on board!"
 }
